ed5fdc3c05f01a75ad2512e6a5ef89d6c69a7e6c
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-09-11 14:31:10 +0300

    ADD table with data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import data from "./data";
+
 const App = () => {
   return (
     <div className="App">
-        App
+        <table>
+            <thead>
+                <th>exchangeType</th>
+                <th>exchangeFrom</th>
+                <th>exchangeTo</th>
+                <th>buy</th>
+                <th>sell</th>
+                <th>diff</th>
+            </thead>
+            <tbody>
+                {data.currencies.map((data, index) =>
+                    <tr key={index}>
+                        <td>{data.exchangeType}</td>
+                        <td>{data.exchangeFrom}</td>
+                        <td>{data.exchangeTo}</td>
+                        <td>{data.buy}</td>
+                        <td>{data.sell}</td>
+                        <td>{data.diff}</td>
+                    </tr>
+
+                )}
+            </tbody>
+        </table>
     </div>
   );
 }
